abbf1672164c28ac8c666c72547dee026f821a67
knightch <amankumarpandey2397@gmail.com>
2023-04-27 00:17:34 +0530

    initial setup

---
+++ src/main/java/com/springbootMasterClass/webapplicationwithspringboot/WebApplicationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springbootMasterClass.webapplicationwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootMasterClass/webapplicationwithspringboot/WebApplicationWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootMasterClass.webapplicationwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
